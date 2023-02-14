e6fba013f5b2a9c079777d05a1fb5a122e876a07
phungo-hub <ngokieuphu@gmail.com>
2023-02-14 22:46:28 +0700

    add customers page

--- src/scenes/products/index.jsx
+++ src/scenes/products/index.jsx
@@ -80,7 +80,7 @@ const Product = ({
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
-  console.log("data: ", data);
+  
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products" />
