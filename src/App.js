ec2afdf682d781005dfa1111487509da73ec1664
phungo-hub <ngokieuphu@gmail.com>
2023-02-15 00:35:02 +0700

    add transactions-page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { createTheme } from "@mui/material"
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { themeSettings } from "theme"
-import Layout from "scenes/layout"
-import Dashboard from "scenes/dashboard"
-import Products from "scenes/products"
-import Customers from "scenes/customers"
+import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -23,6 +24,7 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
             </Route>
           </Routes>
         </ThemeProvider>
